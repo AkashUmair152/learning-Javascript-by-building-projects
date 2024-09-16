9fbfe41729336e6c4793c8fbed7967ab4b8a4f25
Akash Umair <akashumair922@gmail.com>
2024-09-16 09:10:33 +0500

    datatype Conversion

---
+++ ConvrsionOperation/app.js
@@ -0,0 +1,12 @@
+let score = "33";
+// how to convert datatype of a value
+console.log(typeof score);
+
+let valueInScore = Number(score);
+console.log(valueInScore);
+console.log(typeof valueInScore);
+
+// "33"=> 33
+// "33abc"=>NaN
+// true => 1: false =>0
+// null=>0

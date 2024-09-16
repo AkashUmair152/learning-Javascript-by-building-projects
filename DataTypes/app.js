101eb68861d826caef63479f880b1ff52ebc309e
Akash Umair <akashumair922@gmail.com>
2024-09-16 08:54:39 +0500

    basic and datatype in javascript

---
+++ DataTypes/app.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all Javascript code as newer version
+
+// alert(3 + 3);
+
+// code readability should be high  keep allways in mind while writing code
+
+let Name = "Akash";
+let age = 26;
+let isLogedIn = true;
+
+/*
+datatypes in javascript 
+
+number  example=> 2,  34 , 1.3/
+bigint  
+string => ""
+boolean => true or false
+null=> standalone value (empty)
+undefined 
+symbol=> a unique value 
+
+*/
+
+// if you want to know the type of any data you can use
+
+console.log(typeof age);
+console.log(typeof Name);
+console.log(typeof isLogedIn);
---
+++ baisc/app.js
@@ -0,0 +1,18 @@
+console.log("Hello World");
+
+
+// value of const are not allowed to change
+const accountId = 123345;
+
+// value of let can be changed 
+let accountEmail = "akash@gmail.com";
+
+// value of var alos can be changed 
+
+var accountPassword = "123456";
+
+/* 
+prefer to use let instead of var 
+beacuse issue of block scope and funcitonal scope 
+*/
+

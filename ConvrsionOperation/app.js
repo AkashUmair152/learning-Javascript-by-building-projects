e61d5439fc9ee715ce0296192b4c8be7dde80f5e
Akash Umair <akashumair922@gmail.com>
2024-09-16 09:33:02 +0500

    operation

--- ConvrsionOperation/app.js
+++ ConvrsionOperation/app.js
@@ -3,10 +3,38 @@ let score = "33";
 console.log(typeof score);
 
 let valueInScore = Number(score);
-console.log(valueInScore);
-console.log(typeof valueInScore);
+// console.log(valueInScore);
+// console.log(typeof valueInScore);
 
 // "33"=> 33
 // "33abc"=>NaN
 // true => 1: false =>0
 // null=>0
+
+// ++++++++++++++++++ Operation in javascript ++++++++++++++++++++++
+
+let value = 3;
+let nagitiveValue = -value;
+// console.log(nagitiveValue);
+
+/* 
+
+console.log(2+2);
+console.log(2-3);
+console.log(2/3);
+console.log(2*3);
+console.log(2**3); 3 is the power of 2  
+console.log(2%3); remander of  
+
+*/
+
+let str1 = "Hello";
+let str2 = " Akash";
+let str3 = str1 + str2;
+// console.log(str3);
+
+console.log(1 + 2);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + "2" + 3);

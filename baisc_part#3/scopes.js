3586a965a16d6ef4dce496233f7489d3d2b2bb6c
Akash Umair <akashumair922@gmail.com>
2024-09-19 07:02:52 +0500

    iife in javascript

--- baisc_part#3/scopes.js
+++ baisc_part#3/scopes.js
@@ -15,7 +15,7 @@ if (true) {
 
 
 function one(){
-    const username = "hitesh"
+    const username = "akash"
 
     function two(){
         const website = "youtube"
@@ -30,8 +30,8 @@ function one(){
 // one()
 
 if (true) {
-    const username = "hitesh"
-    if (username === "hitesh") {
+    const username = "akash"
+    if (username === "akash") {
         const website = " youtube"
         // console.log(username + website);
     }
